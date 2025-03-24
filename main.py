ef1524fde3a042cbd29b52cd850a057fd5cf39c6
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-24 01:32:36 -0600

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from redis_om import get_redis_connection
+app = FastAPI()
+
+# Connect to Redis
+
+redis_conn = get_redis_connection(
+    host="redis-12894.c1.us-east1-2.gce.redns.redis-cloud.com",
+    port=12894,
+)
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
