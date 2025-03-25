ce56d06a02e2b70da3ed326fa730f8ce883ef96f
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-24 21:02:54 -0600

    fixed consumer when we need to refund

--- consumer.py
+++ consumer.py
@@ -5,20 +5,24 @@ key = 'order_completed'
 group = 'inventory-group'
 
 try:
-    redis_conn.xgroup_create(key, group)
+    redis_conn.xgroup_create(key, group, mkstream=True)
 except:
     print('Group already exists')
 
 while True:
     try:
-        results = redis_conn.xread_group(group, key, {key: '>'}, None)
+        results = redis_conn.xreadgroup(group, key, {key: '>'}, None)
         
         if results != []:
             for result in results:
                 order = result[1][0][1]
                 product = Product.get(order['product_id'])
-                product.quantity -= int(order['quantity'])
-                product.save()
+                try:
+                    print(product)
+                    product.quantity -= int(order['quantity'])
+                    product.save()
+                except:
+                    redis_conn.xadd('refund_order', order)
 
     except Exception as e:
         print(f'Error reading from Redis: {str(e)}')
